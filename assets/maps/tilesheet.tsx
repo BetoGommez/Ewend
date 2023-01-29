48f09b218565b83b067972164ddf16c7014a01c8
AlbertoClase <albertogomezclase@gmail.com>
2023-01-29 12:18:46 +0100

    Added the lasting image resources of the map

---
+++ assets/maps/tilesheet.tsx
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tilesheet" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="Tileset.png" width="256" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="17" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="4">
+   <object id="4" x="15.9375" y="18.8125" width="16.0625" height="13.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0625" y="16.625" width="32" height="15.0625"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="17.125" width="15.875" height="14.9375"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="28.875" width="16.3125" height="3.125"/>
+   <object id="6" x="18.1875" y="22.375" width="12.5" height="6.5"/>
+   <object id="7" x="23.25" y="19.0625" width="2.6875" height="3.5625"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="16" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
