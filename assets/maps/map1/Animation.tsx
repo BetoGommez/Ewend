ea11c1eb3cfe8af9853ef85a487679f9f819f223
AlbertoClase <albertogomezclase@gmail.com>
2023-02-23 01:28:18 +0100

    Added some sounds

---
+++ assets/maps/map1/Animation.tsx
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Animation" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="materials/Terrain/Animated_props.png" width="272" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="26" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="28" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="26" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="26" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="30" duration="250"/>
+   <frame tileid="31" duration="250"/>
+   <frame tileid="32" duration="250"/>
+   <frame tileid="33" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="30" duration="250"/>
+   <frame tileid="31" duration="250"/>
+   <frame tileid="32" duration="250"/>
+   <frame tileid="33" duration="250"/>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="23" duration="250"/>
+   <frame tileid="24" duration="250"/>
+   <frame tileid="25" duration="250"/>
+   <frame tileid="26" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <animation>
+   <frame tileid="34" duration="200"/>
+   <frame tileid="35" duration="200"/>
+   <frame tileid="36" duration="200"/>
+   <frame tileid="37" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <animation>
+   <frame tileid="38" duration="300"/>
+   <frame tileid="39" duration="300"/>
+   <frame tileid="40" duration="300"/>
+   <frame tileid="41" duration="300"/>
+   <frame tileid="42" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="51">
+  <animation>
+   <frame tileid="51" duration="300"/>
+   <frame tileid="52" duration="300"/>
+   <frame tileid="53" duration="300"/>
+   <frame tileid="54" duration="300"/>
+   <frame tileid="55" duration="300"/>
+   <frame tileid="56" duration="300"/>
+   <frame tileid="57" duration="300"/>
+   <frame tileid="58" duration="300"/>
+   <frame tileid="59" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <animation>
+   <frame tileid="68" duration="300"/>
+   <frame tileid="69" duration="300"/>
+   <frame tileid="70" duration="300"/>
+   <frame tileid="71" duration="300"/>
+   <frame tileid="72" duration="300"/>
+   <frame tileid="73" duration="300"/>
+   <frame tileid="74" duration="300"/>
+   <frame tileid="75" duration="300"/>
+   <frame tileid="76" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="77">
+  <animation>
+   <frame tileid="77" duration="200"/>
+   <frame tileid="78" duration="200"/>
+   <frame tileid="79" duration="200"/>
+   <frame tileid="80" duration="200"/>
+   <frame tileid="81" duration="200"/>
+   <frame tileid="82" duration="200"/>
+   <frame tileid="83" duration="200"/>
+   <frame tileid="84" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="85">
+  <animation>
+   <frame tileid="85" duration="300"/>
+   <frame tileid="86" duration="300"/>
+   <frame tileid="87" duration="300"/>
+   <frame tileid="88" duration="300"/>
+   <frame tileid="89" duration="300"/>
+   <frame tileid="90" duration="300"/>
+   <frame tileid="91" duration="300"/>
+   <frame tileid="92" duration="300"/>
+   <frame tileid="93" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="94">
+  <animation>
+   <frame tileid="94" duration="200"/>
+   <frame tileid="95" duration="200"/>
+   <frame tileid="96" duration="200"/>
+   <frame tileid="97" duration="200"/>
+   <frame tileid="98" duration="200"/>
+   <frame tileid="99" duration="200"/>
+   <frame tileid="100" duration="200"/>
+   <frame tileid="101" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="102">
+  <animation>
+   <frame tileid="102" duration="300"/>
+   <frame tileid="103" duration="300"/>
+   <frame tileid="104" duration="300"/>
+   <frame tileid="105" duration="300"/>
+   <frame tileid="106" duration="300"/>
+   <frame tileid="107" duration="300"/>
+   <frame tileid="108" duration="300"/>
+   <frame tileid="109" duration="300"/>
+   <frame tileid="110" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="111">
+  <animation>
+   <frame tileid="111" duration="200"/>
+   <frame tileid="112" duration="200"/>
+   <frame tileid="113" duration="200"/>
+   <frame tileid="114" duration="200"/>
+   <frame tileid="115" duration="200"/>
+   <frame tileid="116" duration="200"/>
+   <frame tileid="117" duration="200"/>
+   <frame tileid="118" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="119">
+  <animation>
+   <frame tileid="119" duration="300"/>
+   <frame tileid="120" duration="300"/>
+   <frame tileid="121" duration="300"/>
+   <frame tileid="122" duration="300"/>
+   <frame tileid="123" duration="300"/>
+   <frame tileid="124" duration="300"/>
+   <frame tileid="125" duration="300"/>
+   <frame tileid="126" duration="300"/>
+   <frame tileid="127" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="128">
+  <animation>
+   <frame tileid="128" duration="200"/>
+   <frame tileid="129" duration="200"/>
+   <frame tileid="130" duration="200"/>
+   <frame tileid="131" duration="200"/>
+   <frame tileid="132" duration="200"/>
+   <frame tileid="133" duration="200"/>
+   <frame tileid="134" duration="200"/>
+   <frame tileid="135" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/maps/map1/Terrain Probs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Terrain Probs" tilewidth="16" tileheight="16" tilecount="950" columns="19">
+ <image source="materials/Terrain/Terrain_and_Props-Old.png" width="304" height="800"/>
+</tileset>
