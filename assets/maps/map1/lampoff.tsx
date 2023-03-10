ece0111f1631804ac8a4da48949c80344a7762a9
AlbertoClase <albertogomezclase@gmail.com>
2023-03-10 23:04:00 +0100

    Added all settings and tutorial

---
+++ assets/maps/map1/lampoff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="lampoff" tilewidth="11" tileheight="22" tilecount="1" columns="1">
+ <image source="materials/Lamp/lamp.png" width="11" height="22"/>
+</tileset>
